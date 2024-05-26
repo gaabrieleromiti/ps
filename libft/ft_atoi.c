9f540cc266d4ada71b0ab6f9625e14df0ceb46b5
gromiti <gromiti@Mark-II>
2024-05-26 16:14:16 +0200

    modified atoi to check if the number is in the integer's range

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -3,26 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gromiti <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: gromiti <gromiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 12:52:43 by gromiti           #+#    #+#             */
-/*   Updated: 2024/01/11 14:36:43 by gromiti          ###   ########.fr       */
+/*   Updated: 2024/05/26 16:13:44 by gromiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_atoi(const char *s)
+int	ft_atoi(const char *s, int *err)
 {
-	int	res;
-	int	sign;
+	long int	res;
+	int			sign;
 
 	res = 0;
 	sign = 1;
 	while ((*s == 32) || ((9 <= *s) && (*s <= 13)))
-	{
 		s++;
-	}
 	if (*s == 45 || *s == 43)
 	{
 		if (*s == 45)
@@ -33,6 +31,11 @@ int	ft_atoi(const char *s)
 	{
 		res = (res * 10) + (*s - 48);
 		s++;
+	}
+	if (res * sign > 2147483647 || res * sign < -2147483648)
+	{
+		*err = 1;
+		return (0);
 	}
 	return (res * sign);
 }
