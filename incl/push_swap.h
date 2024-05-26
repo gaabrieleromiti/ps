9d313984e90b876fe8030d9c54dc0124700d5f40
gromiti <gromiti@Mark-II>
2024-05-26 15:29:53 +0200

    initialized the .h

---
+++ incl/push_swap.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gromiti <gromiti@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/26 15:28:00 by gromiti           #+#    #+#             */
+/*   Updated: 2024/05/26 15:28:59 by gromiti          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h> // REMOVE
+
+#endif
