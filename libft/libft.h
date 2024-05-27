9d5f4988b1030caeffb8ca0731cd884ae4b475cf
gromiti <gromiti@Mark-II>
2024-05-27 17:31:33 +0200

    fixed ft_atoi's parameters in libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: gromiti <gromiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 12:53:18 by gromiti           #+#    #+#             */
-/*   Updated: 2024/05/26 16:14:52 by gromiti          ###   ########.fr       */
+/*   Updated: 2024/05/27 17:06:47 by gromiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *s1, const char *s2, size_t len);
-int		ft_atoi(const char *s, int *err);
+int		ft_atoi(const char *s);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
