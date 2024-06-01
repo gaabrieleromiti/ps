e5230aee9a4181376c8a88a2239e5cb6241fcc26
gromiti <gromiti@Mark-II>
2024-06-01 15:23:52 +0200

    added the sorting till 3 to the .h

--- incl/push_swap.h
+++ incl/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: gromiti <gromiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 15:28:00 by gromiti           #+#    #+#             */
-/*   Updated: 2024/05/27 18:36:39 by gromiti          ###   ########.fr       */
+/*   Updated: 2024/05/28 16:49:25 by gromiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,31 +19,43 @@
 
 # include "../libft/libft.h"
 
+// args.c
 int		max_min_check(char *s);
 int		dupl_check(int argc, char **argv);
 int		dupl_check(int argc, char **argv);
 int		ord_check(int argc, char **argv);
 void	args_check_parse(int argc, char **argv, int *a, int *b);
 
+// free.c
 void	free_err_check(int *a, int *b, int err);
 
+// push.c
 void	slide_down(int *stack, int *elem);
 void	slide_up(int *stack, int *elem);
 void	pa(int *a, int *b, int *a_elem, int *b_elem);
 void	pb(int *a, int *b, int *a_elem, int *b_elem);
 
+// r_rotate.c
 void	rra(int *a, int *a_elem);
 void	rrb(int *b, int *b_elem);
 void	rrr(int *a, int *b, int *a_elem, int *b_elem);
 
+// rotate.c
 void	ra(int *a, int *a_elem);
 void	rb(int *b, int *b_elem);
 void	rr(int *a, int *b, int *a_elem, int *b_elem);
 
+// sort_till_3.c
+void	sort_till_3(int *a, int *b, int *a_elem, int *b_elem);
+
+// sort.c
+void	sort_2(int *a);
+void	sort_3(int *a, int *a_elem);
+void	sort(int *a, int *b, int *a_elem, int *b_elem);
+
+// swap.c
 void	sa(int *a);
 void	sb(int *b);
 void	ss(int *a, int *b);
-
-
 
 #endif
