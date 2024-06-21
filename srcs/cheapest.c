e3a3dcfd953a31f2a637ba1dd1d1323d2d95516c
gromiti <gromiti@Mark-II>
2024-06-21 11:06:55 +0200

    changed some parameters name, added ft_calloc to allocate tot and cheapest inside get_cheapest

--- srcs/cheapest.c
+++ srcs/cheapest.c
@@ -6,24 +6,24 @@
 /*   By: gromiti <gromiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 10:38:57 by gromiti           #+#    #+#             */
-/*   Updated: 2024/06/20 18:18:55 by gromiti          ###   ########.fr       */
+/*   Updated: 2024/06/21 10:51:24 by gromiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/push_swap.h"
-
-void	check_cheapest(int *buff, int *cheapest, int ind)
+// cheapest[0] == tot_moves; cheapest[1] == element; cheapest[2] == move_code;
+void	check_cheapest(int *tot, int *cheapest, int n)
 {
-	int	i;
+	int	mvoe_code;
 
-	i = -1;
-	while (i++ < 4)
+	mvoe_code = -1;
+	while (mvoe_code++ < 4)
 	{
-		if (cheapest[0] > buff[i])
+		if (cheapest[0] > tot[mvoe_code])
 		{
-			cheapest[0] = buff[i];
-			cheapest[1] = ind;
-			cheapest[2] = i;
+			cheapest[0] = tot[mvoe_code];
+			cheapest[1] = n;
+			cheapest[2] = mvoe_code;
 		}
 	}
 }
@@ -31,19 +31,21 @@ void	check_cheapest(int *buff, int *cheapest, int ind)
 int	*get_cheapest(int *a, int *a_n, int *b, int *b_n)
 {
 	int	i;
-	int	buff[4];
-	int	cheapest[3];
+	int	*tot;
+	int	*cheapest;
 
 	i = -1;
+	tot = ft_calloc(4, sizeof(int));
+	cheapest = ft_calloc(3, sizeof(int));
 	while (i++ < *a_n)
 	{
-		buff[0] = get_r_r(index(a[i], a, a_n), spot(a[i], b, b_n));
-		buff[1] = get_rr_r(a_n, index(a[i], a, a_n), spot(a[i], b, b_n));
-		buff[2] = get_rr_rr(a_n, b_n, index(a[i], a, a_n), spot(a[i], b, b_n));
-		buff[3] = get_r_rr(b_n, index(a[i], a, a_n), spot(a[i], b, b_n));
-		check_cheapest(buff, cheapest, i);
+		tot[0] = get_r_r(index(a[i], a, a_n), spot(a[i], b, b_n));
+		tot[1] = get_rr_r(a_n, index(a[i], a, a_n), spot(a[i], b, b_n));
+		tot[2] = get_rr_rr(a_n, b_n, index(a[i], a, a_n), spot(a[i], b, b_n));
+		tot[3] = get_r_rr(b_n, index(a[i], a, a_n), spot(a[i], b, b_n));
+		check_cheapest(tot, cheapest, a[i]);
 	}
-	free(buff);
+	free(tot);
 	return (cheapest);
 }
 
@@ -53,5 +55,12 @@ void	apply_cheapest(int *a, int *a_n, int *b, int *b_n)
 
 	cheapest = get_cheapest(a, a_n, b, b_n);
 	if (cheapest[2] == 0)
-
+		apply_r_r(a, a_n, b, b_n);
+	else if (cheapest[2] == 1)
+		apply_rr_r(a, a_n, b, b_n);
+	else if (cheapest[2] == 2)
+		apply_rr_rr(a, a_n, b, b_n);
+	else if (cheapest[2] == 3)
+		apply_r_rr(a, a_n, b, b_n);
+	free(cheapest);
 }
