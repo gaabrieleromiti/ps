cb3c88d6b95543fe17defa172e6d3201099bfa67
gromiti <gromiti@Mark-II>
2024-07-03 16:18:21 +0200

    it seems to be working

--- srcs/checker/checker.c
+++ srcs/checker/checker.c
@@ -6,67 +6,61 @@
 /*   By: gromiti <gromiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 17:29:19 by gromiti           #+#    #+#             */
-/*   Updated: 2024/06/28 16:39:24 by gromiti          ###   ########.fr       */
+/*   Updated: 2024/07/03 16:15:03 by gromiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incl/push_swap.h"
-#include <stdio.h>
 
-void	check_and_parse(int argc, char **argv, int *a, int *b)
+void	get_apply(char *line, t_stack a, t_stack b)
 {
-	int	i;
-
-	i = 0;
-	if (digit_check(argc, argv) == 2 || dupl_check(argc, argv) == 2)
-		free_err_check(a, b, 2);
-	if (!ord_check(argc, argv))
-		free_err_check(a, b, 0);
-	while (i++ < argc - 1)
-		a[i - 1] = ft_atoi(argv[i]);
-}
-
-void	get_apply(int *a, int *a_n, int *b, int *b_n)
-{
-	char	*line;
-
-	while ((line = get_next_line(0)) != NULL)
-	{
-		if (!ft_strncmp(line, "sa", 2))
-			sa(a);
-		else if (!ft_strncmp(line, "sb", 2))
-			sb(b);
-		else if (!ft_strncmp(line, "ss", 2))
-			ss(a, b);
-		else if (!ft_strncmp(line, "ra", 2))
-			ra(a, a_n);
-		else if (!ft_strncmp(line, "rb", 2))
-			rb(b, b_n);
-		else if (!ft_strncmp(line, "rr", 2))
-			rr(a, b, a_n, b_n);
-		else if (!ft_strncmp(line, "rra", 3))
-			rra(a, a_n);
-		else if (!ft_strncmp(line, "rrb", 3))
-			rrb(b, b_n);
-		else if (!ft_strncmp(line, "rrr", 3))
-			rrr(a, b, a_n, b_n);
-		free(line);
-	}
+	if (ft_strlen(line) == 3 && !ft_strncmp(line, "sa", 2))
+		sa(a.stack);
+	else if (ft_strlen(line) == 3 && !ft_strncmp(line, "sb", 2))
+		sb(b.stack);
+	else if (ft_strlen(line) == 3 && !ft_strncmp(line, "ss", 2))
+		ss(a.stack, b.stack);
+	else if (ft_strlen(line) == 3 && !ft_strncmp(line, "ra", 2))
+		ra(a.stack, a.stack_n);
+	else if (ft_strlen(line) == 3 && !ft_strncmp(line, "rb", 2))
+		rb(b.stack, b.stack_n);
+	else if (ft_strlen(line) == 3 && !ft_strncmp(line, "rr", 2))
+		rr(a.stack, b.stack, a.stack_n, b.stack_n);
+	else if (ft_strlen(line) == 4 && !ft_strncmp(line, "rra", 3))
+		rra(a.stack, a.stack_n);
+	else if (ft_strlen(line) == 4 && !ft_strncmp(line, "rrb", 3))
+		rrb(b.stack, b.stack_n);
+	else if (ft_strlen(line) == 4 && !ft_strncmp(line, "rrr", 3))
+		rrr(a.stack, b.stack, a.stack_n, b.stack_n);
+	else if (ft_strlen(line) == 3 && !ft_strncmp(line, "pa", 2))
+		pa(a.stack, b.stack, a.stack_n, b.stack_n);
+	else if (ft_strlen(line) == 3 && !ft_strncmp(line, "pb", 2))
+		pb(a.stack, b.stack, a.stack_n, b.stack_n);
+	else
+		free_ok_ko(a, b, line, 3);
 }
 
 int	main(int argc, char **argv)
 {
-	int	*a;
-	int	a_n;
-	int	*b;
-	int	b_n;
+	t_stack	a;
+	t_stack	b;
+	char	*line;
 
 	if (argc < 2 || !argv[1])
 		return (0);
-	a = ft_calloc(argc - 1, sizeof(int));
-	a_n = argc - 1;
-	b = ft_calloc(argc - 1, sizeof(int));
-	b_n = 0;
-	args_check_parse(argc, argv, a, b);
-	get_apply(a, &a_n, b, &b_n);
+	a.stack = ft_calloc(argc - 1, sizeof(int));
+	a.stack_n = ft_calloc(1, sizeof(int));
+	b.stack = ft_calloc(argc - 1, sizeof(int));
+	b.stack_n = ft_calloc(1, sizeof(int));
+	*(a.stack_n) = argc - 1;
+	*(b.stack_n) = 0;
+	check_and_parse(argc, argv, a.stack, b.stack);
+	line = get_next_line(0);
+	while (line)
+	{
+		get_apply(line, a, b);
+		free(line);
+		line = get_next_line(0);
+	}
+	check_and_exit(a, b, line);
 }
--- srcs/checker/checker_utils.c
+++ srcs/checker/checker_utils.c
@@ -6,9 +6,74 @@
 /*   By: gromiti <gromiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 17:29:23 by gromiti           #+#    #+#             */
-/*   Updated: 2024/06/27 17:34:32 by gromiti          ###   ########.fr       */
+/*   Updated: 2024/07/03 16:16:39 by gromiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incl/push_swap.h"
 
+void	free_ok_ko(t_stack a, t_stack b, char *line, int err)
+{
+	if (a.stack)
+		free(a.stack);
+	if (b.stack)
+		free(b.stack);
+	if (a.stack_n)
+		free(a.stack_n);
+	if (b.stack_n)
+		free(b.stack_n);
+	if (line)
+		free(line);
+	if (err == 1)
+	{
+		write(1, "OK\n", 3);
+		exit(0);
+	}
+	if (err == 2)
+	{
+		write(1, "KO\n", 3);
+		exit(1);
+	}
+	if (err == 3)
+	{
+		write(1, "Error\n", 6);
+		exit(1);
+	}
+}
+
+void	check_and_parse(int argc, char **argv, int *a, int *b)
+{
+	int	i;
+
+	i = 0;
+	if (digit_check(argc, argv) == 2 || dupl_check(argc, argv) == 2)
+		free_err_check(a, b, 2);
+	if (!ord_check(argc, argv))
+		free_err_check(a, b, 0);
+	while (i++ < argc - 1)
+		a[i - 1] = ft_atoi(argv[i]);
+}
+
+void	check_and_exit(t_stack a, t_stack b, char *line)
+{
+	if (b.stack_n[0] == 0 && is_sorted(a.stack, a.stack_n[0]))
+		free_ok_ko(a, b, line, 1);
+	else if (b.stack_n[0] != 0)
+		free_ok_ko(a, b, line, 2);
+	else if (!is_sorted(a.stack, a.stack_n[0]))
+		free_ok_ko(a, b, line, 2);
+}
+
+int	is_sorted(int *a, int n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n - 1)
+	{
+		if (a[i] > a[i + 1])
+			return (0);
+		i++;
+	}
+	return (1);
+}
