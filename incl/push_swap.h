72d36e9b0c0e01aec2c21cc691e25fa7571014ae
gromiti <gromiti@Mark-II>
2024-07-09 17:42:58 +0200

    norminetted, tested, bonus does not work, to be fixed

--- incl/push_swap.h
+++ incl/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: gromiti <gromiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 15:28:00 by gromiti           #+#    #+#             */
-/*   Updated: 2024/07/09 17:22:49 by gromiti          ###   ########.fr       */
+/*   Updated: 2024/07/09 17:36:47 by gromiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,7 +84,7 @@ void	sort_till_3(int *a, int *b, int *a_n, int *b_n);
 
 // srcs/push_swap/sort.c
 void	sort_2(int *a, int *b);
-void	sort_3(int *a, int *a_n, int *b);
+void	sort_3(int *a, int *a_n, int *b, int *b_n);
 void	sort_4(int *a, int *a_n, int *b, int *b_n);
 void	sort_5(int *a, int *a_n, int *b, int *b_n);
 void	sort(int *a, int *b, int *a_n, int *b_n);
--- srcs/push_swap/sort.c
+++ srcs/push_swap/sort.c
@@ -6,7 +6,7 @@
 /*   By: gromiti <gromiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 15:34:41 by gromiti           #+#    #+#             */
-/*   Updated: 2024/07/09 17:23:14 by gromiti          ###   ########.fr       */
+/*   Updated: 2024/07/09 17:42:11 by gromiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	sort_2(int *a, int *b)
 	free_err_check(a, b, 0);
 }
 
-void	sort_3(int *a, int *a_n, int *b)
+void	sort_3(int *a, int *a_n, int *b, int *b_n)
 {
 	if (a[0] < a[1] && a[1] > a[2] && a[0] < a[2])
 	{
@@ -37,7 +37,7 @@ void	sort_3(int *a, int *a_n, int *b)
 		sa(a);
 		rra(a, a_n);
 	}
-	if (b[0] == 0)
+	if (*b_n == 0)
 		free_err_check(a, b, 0);
 }
 
@@ -51,7 +51,7 @@ void	sort_4(int *a, int *a_n, int *b, int *b_n)
 			rra(a, a_n);
 	}
 	pb(a, b, a_n, b_n);
-	sort_3(a, a_n, b);
+	sort_3(a, a_n, b, b_n);
 	pa(a, b, a_n, b_n);
 }
 
@@ -68,7 +68,7 @@ void	sort_5(int *a, int *a_n, int *b, int *b_n)
 		}
 		pb(a, b, a_n, b_n);
 	}
-	sort_3(a, a_n, b);
+	sort_3(a, a_n, b, b_n);
 	while (*b_n > 0)
 		pa(a, b, a_n, b_n);
 }
@@ -78,7 +78,7 @@ void	sort(int *a, int *b, int *a_n, int *b_n)
 	if (*a_n == 2)
 		sort_2(a, b);
 	else if (*a_n == 3)
-		sort_3(a, a_n, b);
+		sort_3(a, a_n, b, b_n);
 	else if (*a_n == 4)
 		sort_4(a, a_n, b, b_n);
 	else if (*a_n == 5)
