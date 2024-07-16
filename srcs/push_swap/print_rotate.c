3cac1ec46fe16483a2a3df159378302333a9a926
gromiti <gromiti@Mark-II>
2024-07-16 14:31:38 +0200

    norminetted

--- srcs/push_swap/print_rotate.c
+++ srcs/push_swap/print_rotate.c
@@ -6,25 +6,25 @@
 /*   By: gromiti <gromiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 18:02:11 by gromiti           #+#    #+#             */
-/*   Updated: 2024/07/15 18:03:22 by gromiti          ###   ########.fr       */
+/*   Updated: 2024/07/16 14:30:59 by gromiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incl/push_swap.h"
 
-void print_ra(int *a, int *a_n)
+void	print_ra(int *a, int *a_n)
 {
 	ra(a, a_n);
 	write(1, "ra\n", 3);
 }
 
-void print_rb(int *b, int *b_n)
+void	print_rb(int *b, int *b_n)
 {
 	rb(b, b_n);
 	write(1, "rb\n", 3);
 }
 
-void print_rr(int *a, int *b, int *a_n, int *b_n)
+void	print_rr(int *a, int *b, int *a_n, int *b_n)
 {
 	rr(a, b, a_n, b_n);
 	write(1, "rr\n", 3);
